9babd4f841ac2c6ab870340aef17c55a90cdb28c
sawdev10 <0sapphy@gmail.com>
2025-07-06 22:27:21 +0000

    chore: updated ignore folders.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist/', 'node_modules/', 'tests/.node/']),
+  globalIgnores(['dist/', 'node_modules/', 'tests/.node/', 'docs/']),
   {
     files: ['**/*.{ts,mts,cts}'],
     linterOptions: {
